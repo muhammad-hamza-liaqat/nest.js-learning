8642fbf64f4f0a1fb32b08a6e60f6ba55d78f8e8
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-09-30 11:59:47 +0500

    req, res from express

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,12 +3,15 @@ import {
   Controller,
   Get,
   HttpCode,
+  HttpStatus,
   Param,
   Post,
   Query,
   Redirect,
   Req,
+  Res,
 } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 // command to generate the controller is nest g controller --controller_name
 // nest g controller users
@@ -29,19 +32,27 @@ export class UsersController {
   }
 
   @Get('my-profile/:id')
-  @Redirect('https://google.com/')
-  getMyInformation(@Param() record: any): any {
-    console.log('incoming params', record);
-    // return 'hello from params route!'+ record.id;
-    return 'hello from params route!';
+  async getMyInformation(@Param() params: any) {
+    console.log('incoming params', params);
+    // return 'hello from params route!'+ params.id;
+    return 'hello from params route!' + params.id;
   }
 
-  //   this route will redirect to nestjs, if there version and is equal to 5, it will be redirected to version v5
+  // this route will redirect to nestjs, if there version and is equal to 5, it will be redirected to version v5
   @Get('docs')
   @Redirect('https://docs.nestjs.com', 302)
   getDocs(@Query('version') version) {
     if (version && version === '5') {
       return { url: 'https://docs.nestjs.com/v5/' };
     }
+  }
+
+  @Post('user-additional')
+  userAdditionalInformation(@Req() req: Request, @Res() res: Response) {
+    const { name, address, phone } = req.body;
+    console.log('req.body', req.body);
+    res
+      .status(HttpStatus.CREATED)
+      .json({ message: 'user additional information added successfully!' });
   }
 }
