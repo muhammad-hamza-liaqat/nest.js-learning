cbdb9d4b8de98aeda336f64c65b040e6e7f324c3
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-01 16:26:17 +0500

    Swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import * as dotenv from 'dotenv'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function bootstrap () {
+  dotenv.config()
+  const app = await NestFactory.create(AppModule)
+  // swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Apis Collections')
+    .setDescription('the list of api(s)')
+    .setVersion('1.0')
+    .addTag('nest.js')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+  // server
+  await app.listen(process.env.PORT)
 }
-bootstrap();
+bootstrap()
