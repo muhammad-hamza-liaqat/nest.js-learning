493ae3a709b06713e60db08dd9150213e2552e43
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-01 12:22:35 +0500

    fixes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { GlobalExceptionFilter } from './helpers/tryCatch.helper';
 
 async function server() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalFilters(new GlobalExceptionFilter())
   await app.listen(process.env.PORT);
 }
 server();
