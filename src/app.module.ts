1972d4e91021aacda4e4cb4a6d808cf400fe9fe1
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-01 14:54:12 +0500

    mongoose connection done

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersService } from './users/users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'process';
 
 @Module({
   imports: [
@@ -10,8 +13,10 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    // DB connection
+    MongooseModule.forRoot(process.env.MONGODB_URI)
   ],
   controllers: [AppController, UsersController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
