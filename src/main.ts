1253119f222a74d25029ab2b95f5d4911dc9dc09
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-01 17:05:08 +0500

    global validation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import * as dotenv from 'dotenv'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap () {
   dotenv.config()
@@ -15,6 +16,16 @@ async function bootstrap () {
     .build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
+
+  // global pipeline for validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+
   // server
   await app.listen(process.env.PORT)
 }
