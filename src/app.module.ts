29c416b5da41c8a6807edfee127db80615ec2e41
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-02 10:34:11 +0500

    switch to postgresql, changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Auth } from './auth/entities/auth.entity';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USER_NAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Auth],
+      // entities: [__dirname + '/../**/*.entity{.ts,.js}'],  // Glob pattern to load all entity files
+      synchronize: true,
+      autoLoadEntities: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
