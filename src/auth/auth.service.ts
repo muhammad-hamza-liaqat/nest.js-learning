cf870a98630033c007c3b5f45514a923daf0c710
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-02 11:55:38 +0500

    changes in api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,29 +3,31 @@ import {
   HttpCode,
   HttpStatus,
   Injectable,
-} from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { Auth } from './entities/auth.entity';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { hashPassword } from 'src/helpers/bcrypt.helper';
-import { sendResponse } from 'src/helpers/response.helper';
+} from '@nestjs/common'
+import { CreateAuthDto } from './dto/create-auth.dto'
+import { Auth } from './entities/auth.entity'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { hashPassword } from 'src/helpers/bcrypt.helper'
+import { sendResponse } from 'src/helpers/response.helper'
 
 @Injectable()
 export class AuthService {
-  constructor(
+  constructor (
     @InjectRepository(Auth)
     private readonly userModel: Repository<Auth>,
   ) {}
+  async signInUser (createAuthDto: CreateAuthDto) {
+    const { userName, email, password, firstName, lastName } = createAuthDto
 
-  async signInUser(createAuthDto: CreateAuthDto) {
-    const { userName, email, password, firstName, lastName } = createAuthDto;
-    const userExist = await this.userModel.find({ where: { email } });
+    const userExist = await this.userModel.findOne({ where: { email } })
+
     if (userExist) {
-      throw new ConflictException('user already exists in the system!');
+      throw new ConflictException('user already exists in the system!')
     }
-    const hashedPassword = await hashPassword(password);
-    console.log(hashedPassword, 'hashed password generated!');
+
+    const hashedPassword = await hashPassword(password)
+    // console.log(hashedPassword, 'hashed password generated!');
 
     const newUser = await this.userModel.create({
       userName,
@@ -33,14 +35,14 @@ export class AuthService {
       firstName,
       lastName,
       password: hashedPassword,
-    });
+    })
 
-    await this.userModel.save(newUser);
+    await this.userModel.save(newUser)
     let response = sendResponse(
       HttpStatus.CREATED,
       'user created successfully',
       { name: newUser.userName, password: newUser.password },
-    );
-    return response;
+    )
+    return response
   }
 }
