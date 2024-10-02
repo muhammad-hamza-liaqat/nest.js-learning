197afe04753e9f4973786568ee6cd99fbbd21564
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-02 17:27:09 +0500

    dto, entity added

--- src/modules/upload/dto/create-upload.dto.ts
+++ src/modules/upload/dto/create-upload.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUploadDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsUrl } from "class-validator";
+
+export class CreateUploadDto {
+    @ApiProperty()
+    @IsUrl()
+    @IsNotEmpty({message: "image URL can not be empty"})
+    imageUrl: string
+}
--- src/modules/upload/entities/upload.entity.ts
+++ src/modules/upload/entities/upload.entity.ts
@@ -1,1 +1,16 @@
-export class Upload {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("uploads")
+export class Upload {
+    @PrimaryGeneratedColumn("uuid")
+    id: string
+
+    @Column({length: 1000})
+    imageUrl: string
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
+  
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+    updatedAt: Date;
+}
